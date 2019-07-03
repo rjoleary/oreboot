7f1c3b3c397236b886eb313baff6dc95b1a5fe50
Ryan O'Leary <ryanoleary@google.com>
2019-07-02 20:10:47 -0700

    TEST

--- src/mainboard/ast/ast25x0/src/main.rs
+++ src/mainboard/ast/ast25x0/src/main.rs
@@ -12,25 +12,31 @@ use crate::romstage::chain::chain;
 use drivers::model::Driver;
 use drivers::uart::ns16550::NS16550;
 use drivers::wrappers::DoD;
+use core::fmt;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut uarts = [
-        // Only Uart5 is connected.
-        // TODO: PL011::new(0x1E78_3000, 115200),
-        // TODO: PL011::new(0x1E78_D000, 115200),
-        // TODO: PL011::new(0x1E78_E000, 115200),
-        // TODO: PL011::new(0x1E78_F000, 115200),
-        &mut NS16550::new(0x1E78_4000, 115200) as &mut dyn Driver,
-    ];
-    let console = &mut DoD::new(&mut uarts[..]);
-    console.init();
-    console.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
+    //loop {
+        let mut uarts = [
+            // Only Uart5 is connected.
+            // TODO: PL011::new(0x1E78_3000, 115200),
+            // TODO: PL011::new(0x1E78_D000, 115200),
+            // TODO: PL011::new(0x1E78_E000, 115200),
+            // TODO: PL011::new(0x1E78_F000, 115200),
+            &mut NS16550::new(0x1E78_4000, 115200) as &mut dyn Driver,
+        ];
+        let console = &mut DoD::new(&mut uarts[..]);
+        console.init();
+        console.pwrite(b"Welcome to oreboot\r\n", 0).unwrap();
+        loop {}
 
-    cpu::init();
-    let w = &mut print::WriteTo::new(console);
-    asmram::ram(w);
-    chain()
+        //cpu::init();
+        let w = &mut print::WriteTo::new(console);
+        //fmt::write(w, format_args!("{}", 7));
+        //asmram::ram(w);
+    //}
+
+    //chain()
     //romstage::romstage()
 }
 use core::panic::PanicInfo;
